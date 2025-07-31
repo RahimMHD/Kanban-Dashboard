b4b0e69bb23b2b104b4a7de9abdf82dd2af81910
RahimMHD <mahdirahim0107@gmail.com>
2025-07-31 11:37:23 +0100

    add bar charts

---
+++ src/sections/AllBarComponent/dashboard/data & fun dash/barChartDah.tsx
@@ -0,0 +1,66 @@
+
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar } from 'recharts';
+
+const dataBar: any[] = [
+    {
+        name: 'S',
+        uv: 1400,
+        pv: 2200,
+        amt: 2400,
+    },
+    {
+        name: 'M',
+        uv: 2000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'T',
+        uv: 1700,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'W',
+        uv: 2180,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'T',
+        uv: 3890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'F',
+        uv: 1790,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'S',
+        uv: 2490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
+export default function BarChartWeeklySales() {
+
+    const checkTheBig = dataBar.filter(a => {
+        return [a.uv] 
+    })
+
+    console.log(checkTheBig)
+
+    return (
+    <ResponsiveContainer width="100%" height="100%">
+        <BarChart width={100} height={300} data={dataBar}>
+            <Bar dataKey="uv" fill="#42B1FB" strokeWidth={5} />
+            <XAxis dataKey="name" strokeOpacity={0} stroke='#999' />
+        </BarChart>
+    </ResponsiveContainer>
+    );
+}
---
+++ src/sections/AllBarComponent/dashboard/data & fun dash/dashData.tsx
@@ -0,0 +1,54 @@
+
+
+
+
+export const MeetingSchedule = [
+    {
+        image: "../../../../public/images/avatars/1.png",
+        name: "Call With Woods",
+        description: "21 Jul | 08:20-10:30",
+        badge: "Business",
+        color: "text-blue-700",
+        background: "bg-blue-300"
+    },
+    {
+        image: "../../../../public/images/avatars/2.png",
+        name: "Call with Hilda",
+        description: "24 Jul | 11:30-12:00",
+        badge: "Meditation",
+        color: "text-green-700",
+        background: "bg-green-300"
+    },
+    {
+        image: "../../../../public/images/avatars/3.png",
+        name: "Conference call",
+        description: "28 Jul | 05:00-6:45",
+        badge: "Dinner",
+        color: "text-yellow-700",
+        background: "bg-yellow-300"
+    },
+    {
+        image: "../../../../public/images/avatars/4.png",
+        name: "Meeting with Mark",
+        description: "14 Aug | 04:15-05:30",
+        badge: "Meetup",
+        color: "text-gray-700",
+        background: "bg-gray-300"
+    },
+    {
+        image: "../../../../public/images/avatars/5.png",
+        name: "Meeting in Oakland",
+        description: "21 Jul | 08:20-10:30",
+        badge: "Dinner",
+        color: "text-red-700",
+        background: "bg-red-300"
+    },
+    {
+        image: "../../../../public/images/avatars/6.png",
+        name: "Meeting with Carl",
+        description: "05 Oct | 10:00-12:45",
+        badge: "Business",
+        color: "text-orange-700",
+        background: "bg-orange-300"
+    },
+]
